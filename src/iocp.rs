b9e0a7ba306e48237de81687ed2edc9102e4342d
jtnunley <jtnunley01@gmail.com>
2022-08-08 21:24:54 -0700

    windows changes

--- src/iocp.rs
+++ src/iocp.rs
@@ -2,8 +2,12 @@
 
 #![cfg(windows)]
 
-use std::marker::PhantomData;
-use windows_sys::Win32::System::IO::OVERLAPPED;
+use slab::Slab;
+use std::{cell::UnsafeCell, marker::PhantomData, fmt, sync::{Arc, Weak, Mutex}};
+use windows_sys::Win32::{
+    System::IO::OVERLAPPED,
+    Foundation::HANDLE,
+};
 
 /// This `PollData` exposes an `OVERLAPPED` structure, which is used to
 /// coordinate I/O operations with the OS.
@@ -11,4 +15,67 @@ use windows_sys::Win32::System::IO::OVERLAPPED;
 pub struct OpData<'a> {
     pub(crate) overlapped: *mut OVERLAPPED,
     _marker: PhantomData<'a>,
+}
+
+pub(crate) struct Completion {
+    /// The handle to the IOCP port.
+    iocp_port: HANDLE,
+    /// A buffer for holding active operations.
+    /// 
+    /// This is implied to be stable. Empty slots are owned by the
+    /// completion object, while each `OpEntry` full slot is owned
+    /// by the in-progress operation. Changing a slot from empty to
+    /// full or vice versa requires locking the mutex.
+    /// 
+    /// The capacity should never change, since `OpEntry` does not
+    /// have a stable deref.
+    active_ops: UnsafeCell<Slab<OpEntry>>,
+    /// An exclusion lock for changes to the `active_ops` slab.
+    mutation_lock: Mutex<()>,
+}
+
+impl fmt::Debug for Completion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("Completion")
+            .field("active_ops", &self.active_ops.len())
+            .finish_non_exhaustive()
+    }
+}
+
+/// An entry in the active ops list used to keep track of the
+/// state of an operation.
+#[repr(C)]
+struct OpEntry {
+    /// The OVERLAPPED entry used in this operation.
+    /// 
+    /// Thanks to `repr(C)`, this always comes first, meaning a pointer
+    /// to our `OpEntry` is the same as a pointer to the `OVERLAPPED`
+    /// and vice versa.
+    overlapped: OVERLAPPED,
+    /// The event ID for this operation.
+    key: u64,
+}
+
+impl Completion {
+    /// Create a new completion object.
+    pub fn new() -> Result<Self> {
+        let iocp_port = unsafe {
+            CreateIoCompletionPort(
+                INVALID_HANDLE_VALUE,
+                null_mut(),
+                0,
+                1,
+            )
+        };
+
+        if iocp_port == INVALID_HANDLE_VALUE {
+            return Err(io::Error::last_os_error()); 
+        }
+        
+        Completion {
+            iocp_port,
+            active_ops: UnsafeCell::new(Slab::with_capacity(capacity)),
+            mutation_lock: Mutex::new(()),
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,8 @@ cfg_if::cfg_if! {
 mod source;
 pub use source::{Raw, Source, SourceType};
 
-use std::io::Result;
+use std::{fmt, io::Result, time::Duration};
+use ops::Op;
 
 #[doc(hidden)]
 pub use platform::OpData;
@@ -66,4 +67,59 @@ type PollingFn = Box<dyn FnMut() -> Result<usize> + Send + Sync + 'static>;
 pub struct Event {
     key: u64,
     result: Result<usize>,
+}
+
+/// The interface to system faculties for polling for completion on
+/// certain events.
+pub struct Completion {
+    inner: platform::Completion,
+}
+
+impl fmt::Debug for Completion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&self.inner, f)
+    }
+}
+
+impl Completion {
+    /// Create a new `Completion` instance with the specified capacity.
+    pub fn new(capacity: usize) -> Result<Self> {
+        platform::Completion::new(capacity).map(Into::into)
+    }
+
+    /// Register a source with the completion.
+    pub fn register(&self, source: &impl Source) -> Result<()> {
+        self.inner.register(source)
+    }
+
+    /// Deregister a source from the completion.
+    pub fn deregister(&self, source: &impl Source) -> Result<()> {
+        self.inner.deregister(source)
+    }
+
+    /// Submit an operation to the completion queue.
+    /// 
+    /// # Safety
+    /// 
+    /// Cannot submit the same `op` more than once.
+    pub unsafe fn submit(&self, op: &mut impl Op, key: u64) -> Result<()> {
+        self.inner.submit(op, key)
+    }
+
+    /// Wait for events to be available.
+    pub fn wait(&self, timeout: Option<Duration>, out: &mut Vec<Event>) -> Result<usize> {
+        self.inner.wait(timeout, out)
+    }
+
+    /// Notify the completion, either interrupting a wait cycle or
+    /// pre-empting the next wait cycle.
+    pub fn notify(&self) -> Result<()> {
+        self.inner.notify()
+    }
+}
+
+impl From<platform::Completion> for Completion {
+    fn from(inner: platform::Completion) -> Self {
+        Completion { inner } 
+    }
 }
